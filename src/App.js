b00fb4350a777779d332d1e98bb81efbe2bec994
golestan <golestan900@gmail.com>
2021-12-18 10:22:19 +0330

    Completed counter with redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Counter: {counter}</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button onClick={() => dispatch({type:'INCREMENT'})}>INCREMENT</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <button onClick={() => dispatch({type:'DECREMENT'})}>DECREMENT</button>
+        </p>
       </header>
     </div>
   );
